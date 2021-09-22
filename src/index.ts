f78ace14e84a375b9eca2fea73b302823a4fac3b
Tyler Long <tyler4long@gmail.com>
2021-09-22 16:26:38 -0700

    cv.Mat is undefined

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import cv from './opencv.js';
 import windowButtons from '../images/window-buttons.png';
 
 console.log(cv);
+console.log(cv.Mat);
 
 const canvas = document.createElement('canvas');
 document.body.appendChild(canvas);
@@ -11,5 +12,6 @@ image.onload = () => {
   canvas.width = image.width;
   canvas.height = image.height;
   context.drawImage(image, 0, 0, image.width, image.height);
+  const source = cv.imread(canvas);
 };
 image.src = windowButtons;
