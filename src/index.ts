36a3d360774a8fa56205854b6d6f34d8f80281b6
Tyler Long <tyler4long@gmail.com>
2021-09-22 18:20:37 -0700

    Migrate another sample to webpack

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import openCV from './opencv.js';
 import windowButtons from '../images/window-buttons.png';
+import singleButton from '../images/single-button.png';
 
 openCV.onRuntimeInitialized = () => {
   const image = new Image();
@@ -30,7 +31,6 @@ openCV.onRuntimeInitialized = () => {
       const y = circles.data32F[i * 3 + 1];
       const radius = circles.data32F[i * 3 + 2];
       const center = new openCV.Point(x, y);
-      console.log(center);
       openCV.circle(dest, center, radius, color);
     }
     const canvas = document.createElement('canvas');
@@ -38,10 +38,36 @@ openCV.onRuntimeInitialized = () => {
     document.body.appendChild(canvas);
     openCV.imshow('mycanvas', dest);
 
-    // clean up
-    source.delete();
     dest.delete();
     circles.delete();
+
+    const image2 = new Image();
+    image2.src = singleButton;
+    image2.onload = () => {
+      const source = openCV.imread(image);
+      const template = openCV.imread(image2);
+
+      const dest = new openCV.Mat();
+      const mask = new openCV.Mat();
+      openCV.matchTemplate(source, template, dest, openCV.TM_CCOEFF, mask);
+      const result = openCV.minMaxLoc(dest, mask);
+      const maxPoint = result.maxLoc;
+      const color = new openCV.Scalar(255, 0, 0, 255);
+      const point = new openCV.Point(
+        maxPoint.x + template.cols,
+        maxPoint.y + template.rows
+      );
+      openCV.rectangle(source, maxPoint, point, color, 2, openCV.LINE_8, 0);
+      const canvas = document.createElement('canvas');
+      canvas.id = 'mycanvas2';
+      document.body.appendChild(canvas);
+      openCV.imshow('mycanvas2', source);
+
+      // clean up
+      source.delete();
+      dest.delete();
+      mask.delete();
+    };
   };
   image.src = windowButtons;
 };
